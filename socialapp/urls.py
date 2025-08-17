5c15814a70d26ce2829023a86117a636e6900e28
Gerry-01 <odhiambogerry010@gmail.com>
2025-08-17 23:09:28 +0300

    added the various urls

---
+++ socialapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register', views.register_user, name='register'),
+    path('completeregistration', views.complete_registration, name='completeregistration'),
+    path('login', views.login_user, name='login'),
+    path('userdata', views.user_data, name='userdata'),
+]
