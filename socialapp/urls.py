04d72daa38a07f556003f7be6d2299af38dd1667
Gerry-01 <odhiambogerry010@gmail.com>
2025-08-21 18:38:59 +0300

    added the url for fetching the comments

--- socialapp/urls.py
+++ socialapp/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('deletepost/<int:post_id>', views.delete_post, name='deletepost'),
     path('editpost/<int:post_id>', views.edit_post, name='editpost'),
     path('likepost/<int:post_id>', views.like_post, name='likepost'),
+    path('addcomment/<int:post_id>', views.add_comment, name='addcomment'),
+    path('getcomments/<int:post_id>', views.get_comments, name='getcomments'),
 ]
