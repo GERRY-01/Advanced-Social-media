a52a04e628b2848266d06b16f9bff5786992c94b
Gerry-01 <odhiambogerry010@gmail.com>
2025-08-23 19:52:28 +0300

    added the app js file

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import { BrowserRouter as Router, Route, Routes, RouterProvider } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import Stories from "./Components/Stories";
+import Posts from "./Components/Posts";
+import MobileMenu from "./Components/MobileMenu";
+import Greeting from "./Components/Greeting";
+import './App.css';
+import Suggestions from "./Components/Suggestion";
+import CompleteRegistration from "./Components/CompleteRegistration";
+import ReelsPage from "./Components/Reels";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="" element={
+          <div>
+          <Navbar/> <Sidebar/> <MobileMenu/> <Greeting/> <Stories/> 
+          <Posts/> <Suggestions/>
+          </div>} />
+        <Route path="/completeregistration" element={<CompleteRegistration />} />
+      </Routes>
+    </Router>
+     
+    
+  )
+}
+
+export default App;
